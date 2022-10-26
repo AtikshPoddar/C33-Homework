ff3256f06eccd70c15e41396d773024475250e4a
AtikshPoddar <106470100+AtikshPoddar@users.noreply.github.com>
2022-10-26 17:45:27 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -184,8 +184,8 @@ function handlePlayerArrowCollision() {
 
       /**Update the code here so that computer life 
       reduces if player's arrow hits the target***/
-      ComputerArcherlife -= 1;
-      computer.reduceLife(ComputerArcherLife);
+      computerArcherLife -= 1;
+      computer.reduceLife(computerArcherLife);
 
       if (computerArcherLife <= 0) {
         computerArcher.collapse = true;
